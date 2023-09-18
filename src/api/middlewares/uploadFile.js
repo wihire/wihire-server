8928cc04e8414facad194c7b03dda4f78ed3b986
Jovanka Samudra <samudrajovanka20@gmail.com>
2023-09-19 01:11:33 +0700

    feat(cloudinary): add function for upload to cloudinary

---
+++ src/api/middlewares/uploadFile.js
@@ -0,0 +1,48 @@
+const multer = require('multer');
+
+const InvariantError = require('../../exceptions/InvariantError');
+
+/**
+ * @description accept file format
+ */
+const fileFilter = (acceptedFormat) => (req, file, callback) => {
+  if (acceptedFormat.includes(file.mimetype)) {
+    callback(null, true);
+  } else {
+    callback(new InvariantError('File format is not supported'));
+  }
+};
+
+const uploadImage = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 1 * 1024 * 1024, // 1mb
+  },
+  fileFilter: fileFilter(['image/png', 'image/jpg', 'image/jpeg']),
+});
+
+const upload = (type) => (engine, option) => (req, res, next) => {
+  let uploadEngine;
+  switch (type) {
+    case 'image':
+      uploadEngine = uploadImage;
+      break;
+  }
+
+  uploadEngine[engine](...option)(req, res, (err) => {
+    if (err) {
+      console.log(err);
+      if (err instanceof multer.MulterError) {
+        if (err.code === 'LIMIT_UNEXPECTED_FILE') {
+          next(new InvariantError('File you upload exceeds the maximum limit'));
+        }
+      }
+
+      next(err);
+    }
+
+    next();
+  });
+};
+
+module.exports = upload;
