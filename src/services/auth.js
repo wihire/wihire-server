79a303952ff898364ac93eb47743399ec1305d66
Rusdi Ahmad Azwari <124493783+rusdaii@users.noreply.github.com>
2023-09-19 22:09:35 +0700

    feat: create API register for user and company (#8)
    - add @unique name for CompanyScope table
    - add feature register for company & user
    
    ---------
    
    Co-authored-by: rusdaii <rusdaii.html@gmail.com>

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,9 +1,12 @@
 const bcrypt = require('bcrypt');
-
+const { uniqueSlug } = require('../lib/common');
 const prisma = require('../lib/prisma');
 const AuthtenticationError = require('../exceptions/AuthenticationError');
 const { generateAccessToken } = require('../lib/tokenManager');
-
+const ClientError = require('../exceptions/ClientError');
+const { CONFLICT_ERR } = require('../constants/errorType');
+const NotFoundError = require('../exceptions/NotFoundError');
+const { NOT_FOUND_ERR } = require('../constants/errorType');
 class AuthService {
   static login = async ({ email, password }) => {
     const profile = await prisma.profile.findUnique({
@@ -35,6 +38,133 @@ class AuthService {
         isVerifiedEmail: profile.isVerifiedEmail,
       },
     };
+  };
+
+  static registerUser = async (userData) => {
+    const profile = await prisma.profile.findUnique({
+      where: {
+        email: userData.email,
+      },
+    });
+
+    if (profile) {
+      throw new ClientError('Email already exists', { statusCode: 409, type: CONFLICT_ERR });
+    }
+
+    const hashedPassword = await bcrypt.hash(userData.password, 10);
+
+    const newProfile = await prisma.profile.create({
+      data: {
+        slug: uniqueSlug(userData.name).toLowerCase(),
+        name: userData.name,
+        email: userData.email,
+        password: hashedPassword,
+        role: 'USER',
+        province: userData.province,
+        address: userData.address,
+        user: {
+          create: {
+            birthDate: userData.birthDate,
+            gender: userData.gender,
+            phoneNumber: userData.phoneNumber,
+            salary: {
+              create: {
+                min: userData.salaryExpectation,
+              },
+            },
+          },
+        },
+      },
+      include: {
+        user: {
+          include: {
+            salary: true,
+          },
+        },
+      },
+    });
+
+    return newProfile;
+  };
+
+  static registerCompany = async (companyData) => {
+    const profile = await prisma.profile.findUnique({
+      where: {
+        email: companyData.email,
+      },
+    });
+
+    if (profile) {
+      throw new ClientError('Email already exists', { statusCode: 409, type: CONFLICT_ERR });
+    }
+
+    const hashedPassword = await bcrypt.hash(companyData.password, 10);
+
+    const companyScope = await prisma.companyScope.upsert({
+      where: {
+        name: companyData.companyScope,
+      },
+      create: {
+        name: companyData.companyScope,
+      },
+      update: {},
+    });
+
+    if (!companyScope) {
+      throw new NotFoundError('Company scope not found', {
+        statusCode: 404,
+        type: NOT_FOUND_ERR,
+      });
+    }
+
+    const companyTotalEmployee = await prisma.companyTotalEmployee.findUnique({
+      where: {
+        id: companyData.totalEmployee,
+      },
+    });
+
+    if (!companyTotalEmployee) {
+      throw new NotFoundError('Company total employee not found', {
+        statusCode: 404,
+        type: NOT_FOUND_ERR,
+      });
+    }
+
+    const newProfile = await prisma.profile.create({
+      data: {
+        slug: uniqueSlug(companyData.name).toLowerCase(),
+        name: companyData.name,
+        email: companyData.email,
+        password: hashedPassword,
+        role: 'COMPANY',
+        province: companyData.province,
+        address: companyData.address,
+        company: {
+          create: {
+            companyScope: {
+              connect: {
+                id: companyScope.id,
+              },
+            },
+            companyTotalEmployee: {
+              connect: {
+                id: companyTotalEmployee.id,
+              },
+            },
+          },
+        },
+      },
+      include: {
+        company: {
+          include: {
+            companyScope: true,
+            companyTotalEmployee: true,
+          },
+        },
+      },
+    });
+
+    return newProfile;
   };
 }
 
