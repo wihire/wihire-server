501c2c7cbd292915b6de4e460da48cc7acba506c
Jovanka Samudra <51015175+samudrajovanka@users.noreply.github.com>
2023-09-19 11:55:38 +0700

    feat: add firebase and cloudinary configuration for upload file (#6)

---
+++ src/lib/firebase/config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+const { initializeApp } = require('firebase/app');
+const { getStorage } = require('firebase/storage');
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+exports.storage = getStorage(app);
