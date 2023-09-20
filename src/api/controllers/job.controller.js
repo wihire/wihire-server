4134c193e0db3d2d8c068241eeff0bf40ad47953
Jovanka Samudra <samudrajovanka20@gmail.com>
2023-09-20 18:04:37 +0700

    fix: resolve conflict job route and controller

--- src/api/controllers/job.controller.js
+++ src/api/controllers/job.controller.js
@@ -1,7 +1,41 @@
 const { successResponse } = require('../../lib/response');
+const { getPaginationStatus } = require('../../lib/pagination');
+const ApplicantService = require('../../services/applicant');
+const { isNumber } = require('../../lib/common');
 const JobService = require('../../services/job');
 
 class JobController {
+  static getApplicants = async (req, res, next) => {
+    const { slug } = req.params;
+    let { page, limit } = req.query;
+
+    if (!page || !isNumber(page)) page = 1;
+    if (!limit || !isNumber(limit)) limit = 15;
+
+    try {
+      const applicants = await ApplicantService.getApplicants({
+        jobSlug: slug,
+        page,
+        limit,
+      });
+      const totalData = await ApplicantService.getApplicantTotal(slug);
+
+      const pagination = getPaginationStatus(page, limit, totalData);
+
+      return res.status(200).json(
+        successResponse({
+          message: 'Retrieved user data successfully',
+          data: {
+            applicants,
+          },
+          pagination,
+        }),
+      );
+    } catch (error) {
+      next(error);
+    }
+  };
+
   static saveJob = async (req, res, next) => {
     try {
       const { slug: jobSlug } = req.params;
--- src/api/routes/applicants.route.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ApplicantsController = require('../controllers/applicants.controller');
-
-router.get('/:slug/applicants', ApplicantsController.allApplicantsController);
-
-module.exports = router;
