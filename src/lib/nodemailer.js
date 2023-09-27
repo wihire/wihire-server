c3d928a5aa8b71acd596b25dc2ed56c8c2899b1a
Rusdi Ahmad Azwari <124493783+rusdaii@users.noreply.github.com>
2023-09-27 08:34:16 +0700

    fix: fix nodemailer OAuth token & add googleapis library (#29)
    Co-authored-by: rusdaii <rusdaii.html@gmail.com>

--- src/lib/nodemailer.js
+++ src/lib/nodemailer.js
@@ -1,19 +1,45 @@
 const nodemailer = require('nodemailer');
+const { google } = require('googleapis');
+const OAuth2 = google.auth.OAuth2;
 
-exports.transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    type: 'OAuth2',
-    user: process.env.MAIL_USERNAME,
-    pass: process.env.MAIL_PASSWORD,
-    clientId: process.env.OAUTH_CLIENTID,
-    clientSecret: process.env.OAUTH_CLIENT_SECRET,
-    refreshToken: process.env.OAUTH_REFRESH_TOKEN,
-  },
-});
+const createTransporter = async () => {
+  const oauth2Client = new OAuth2(
+    process.env.OAUTH_CLIENTID,
+    process.env.OAUTH_CLIENT_SECRET,
+    'https://developers.google.com/oauthplayground',
+  );
+
+  oauth2Client.setCredentials({
+    refresh_token: process.env.OAUTH_REFRESH_TOKEN,
+  });
+
+  const accessToken = await new Promise((resolve, reject) => {
+    oauth2Client.getAccessToken((err, token) => {
+      if (err) {
+        reject();
+      }
+      resolve(token);
+    });
+  });
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: process.env.MAIL_USERNAME,
+      accessToken,
+      clientId: process.env.OAUTH_CLIENTID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      refreshToken: process.env.OAUTH_CLIENT_SECRET,
+    },
+  });
+
+  return transporter;
+};
 
 exports.sendEmail = async (options) => {
-  const sendMailData = await this.transporter.sendMail({
+  const transporter = await createTransporter();
+  const sendMailData = await transporter.sendMail({
     from: process.env.MAIL_USERNAME,
     to: options.to,
     subject: options.subject,
