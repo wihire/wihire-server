331f2736e763b998c1527b29782d0be22638e41b
Jovanka Samudra <samudrajovanka20@gmail.com>
2023-09-30 11:12:36 +0700

    fix: make get application descending by created at

--- src/services/application.js
+++ src/services/application.js
@@ -36,6 +36,9 @@ class ApplicationService {
       },
       skip: (page - 1) * limit,
       take: limit,
+      orderBy: {
+        createdAt: 'desc',
+      },
     });
 
     applications.forEach((application) => {
