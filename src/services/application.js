b9e83068f9b950a0aa60f78e921e5f83a6f81fb6
Jovanka Samudra <51015175+samudrajovanka@users.noreply.github.com>
2023-10-01 00:20:39 +0700

    feat: add API for check status applicant (#33)

--- src/services/application.js
+++ src/services/application.js
@@ -1,3 +1,4 @@
+const NotFoundError = require('../exceptions/NotFoundError');
 const prisma = require('../lib/prisma');
 const JobService = require('./job');
 
@@ -60,6 +61,34 @@ class ApplicationService {
     });
 
     return totalApplication;
+  };
+
+  static checkApplication = async ({ userId, jobSlug }) => {
+    const job = await prisma.job.findFirst({
+      where: {
+        slug: jobSlug,
+      },
+    });
+
+    if (!job) {
+      throw new NotFoundError('Job not found');
+    }
+
+    const application = await prisma.applicationList.findFirst({
+      where: {
+        userId,
+        jobId: job.id,
+      },
+    });
+
+    if (!application) {
+      throw new NotFoundError('Application not found');
+    }
+
+    delete application.userId;
+    delete application.jobId;
+
+    return application;
   };
 }
 
